c430f6a115d26e2a6ffdd0361d7237f98b73bffa
ThibMN <thibaudmineau@gmail.com>
2025-01-25 13:25:21 +0100

    test mais pas concluant

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import TodoController from './Controller/todo-controller';
-
 import './index.scss';
+import Router from './Router';
+import ControllerError404 from './Controllers/error-404';
+import ControllerLatestFilm from './Controllers/latest-film';
 
-document.addEventListener('DOMContentLoaded', () => {
-  new TodoController();
-});
+const routes = [
+  { url: '/latest-film', controller: ControllerLatestFilm },
+];
 
+const router = new Router(routes);
+
+export default router;
